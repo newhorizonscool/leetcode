b1e2226118c688092e955a3b721e7ac48274b63a
newhorizonscool <evnevzoroff@yandex.ru>
2022-11-29 16:26:03 +0300

    231. Power of Two

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -1,1 +1,19 @@
+nums = [0,1,0,3,12]
+# answer: [1,3,12,0,0]
+#for i in range(len(nums)):
+   # if (nums[i] == 0 and i < len(nums) -1):
+        #nums[i], nums[i+1] = nums[i+1], nums[i]
+i = 0
+while i <= len(nums):
+    if (nums[i] == 0 and i <= len(nums) - 1):
+        nums[i], nums[i+1] = nums[i+1], nums[i]
+        i += 1
+    if i == len(nums) - 1:
+        break
+    print(nums)
+print(nums)
+
+
+
+        
 
