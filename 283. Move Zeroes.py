1461c7ebbb3595ea92f43242be2dc594c06e96a1
newhorizonscool <evnevzoroff@yandex.ru>
2022-12-05 00:46:34 +0300

    Update 283. Move Zeroes.py

--- 283. Move Zeroes.py
+++ 283. Move Zeroes.py
@@ -3,15 +3,15 @@ nums = [0,1,0,3,12]
 #for i in range(len(nums)):
    # if (nums[i] == 0 and i < len(nums) -1):
         #nums[i], nums[i+1] = nums[i+1], nums[i]
-i = 0
-while i <= len(nums):
+#i = 0
+#while i <= len(nums):
     if (nums[i] == 0 and i <= len(nums) - 1):
         nums[i], nums[i+1] = nums[i+1], nums[i]
         i += 1
     if i == len(nums) - 1:
         break
     print(nums)
-print(nums)
+#print(nums)
 
 
 
